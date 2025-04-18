34d803c0483549c2d9768a6cce09a30b11e10732
Mohamed Adel <mohamedadel1994825@gmail.com>
2025-04-19 01:55:29 +0200

    add about and contact to public paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 // List of public paths that don't require authentication
-const publicPaths = ['/login', '/register'];
+const publicPaths = ['/login', '/register','/about','/contact'];
 
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
